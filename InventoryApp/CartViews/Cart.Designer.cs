84d75278ec4dbb5a8f3fa97411bb68f133f50fbf
EdisonP <133362311+edions@users.noreply.github.com>
2023-05-21 12:49:38 +0800

    Fixed transactions button anchor properties

--- InventoryApp/CartViews/Cart.Designer.cs
+++ InventoryApp/CartViews/Cart.Designer.cs
@@ -93,7 +93,7 @@
             // 
             // button3
             // 
-            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.button3.Location = new System.Drawing.Point(660, 415);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(102, 30);
