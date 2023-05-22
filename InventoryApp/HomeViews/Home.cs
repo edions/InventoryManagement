83bc5163be2adad44776a52f1784eb28f16b33d8
EdisonP <133362311+edions@users.noreply.github.com>
2023-05-22 19:01:52 +0800

    same item now stack if added to cart

--- InventoryApp/HomeViews/Home.cs
+++ InventoryApp/HomeViews/Home.cs
@@ -180,25 +180,59 @@ namespace InventoryApp
         //DATAGRIDVIEW BUTTON EVENT - Home
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-            if (dataGridView1.Columns[e.ColumnIndex] is DataGridViewButtonColumn &&
-                e.RowIndex >= 0)
+            if (dataGridView1.Columns[e.ColumnIndex] is DataGridViewButtonColumn && e.RowIndex >= 0)
             {
                 // Get the values from the selected row
                 string name = dataGridView1.Rows[e.RowIndex].Cells["Name"].Value.ToString();
                 int price = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells["Price"].Value);
 
-                // Save the values in the "Cart" database
+                // Check if the item already exists in the cart
                 using (SqlConnection con = ConnectionManager.GetConnection())
                 {
                     con.Open();
-                    string insertQuery = "INSERT INTO Cart (Name, Price) VALUES (@Name, @Price)";
+                    string selectQuery = "SELECT Quantity, Price FROM Cart WHERE Name = @Name";
 
-                    using (SqlCommand command = new SqlCommand(insertQuery, con))
+                    using (SqlCommand selectCommand = new SqlCommand(selectQuery, con))
                     {
-                        command.Parameters.AddWithValue("@Name", name);
-                        command.Parameters.AddWithValue("@Price", price);
-                        command.ExecuteNonQuery();
+                        selectCommand.Parameters.AddWithValue("@Name", name);
+                        SqlDataReader reader = selectCommand.ExecuteReader();
+
+                        if (reader.Read())
+                        {
+                            // Item already exists in the cart, update the quantity and price
+                            int existingQuantity = Convert.ToInt32(reader["Quantity"]);
+                            int existingPrice = Convert.ToInt32(reader["Price"]);
+                            int newQuantity = existingQuantity + 1;
+                            int newPrice = existingPrice + price;
+
+                            reader.Close();
+
+                            string updateQuery = "UPDATE Cart SET Quantity = @Quantity, Price = @Price WHERE Name = @Name";
+
+                            using (SqlCommand updateCommand = new SqlCommand(updateQuery, con))
+                            {
+                                updateCommand.Parameters.AddWithValue("@Quantity", newQuantity);
+                                updateCommand.Parameters.AddWithValue("@Price", newPrice);
+                                updateCommand.Parameters.AddWithValue("@Name", name);
+                                updateCommand.ExecuteNonQuery();
+                            }
+                        }
+                        else
+                        {
+                            reader.Close();
+
+                            // Item does not exist in the cart, insert a new row
+                            string insertQuery = "INSERT INTO Cart (Name, Price, Quantity) VALUES (@Name, @Price, 1)";
+
+                            using (SqlCommand insertCommand = new SqlCommand(insertQuery, con))
+                            {
+                                insertCommand.Parameters.AddWithValue("@Name", name);
+                                insertCommand.Parameters.AddWithValue("@Price", price);
+                                insertCommand.ExecuteNonQuery();
+                            }
+                        }
                     }
+
                     con.Close();
                 }
 
