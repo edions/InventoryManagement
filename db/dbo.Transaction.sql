87c557f9da1538b010ff9d65853129acc4f6e302
EdisonP <133362311+edions@users.noreply.github.com>
2023-06-05 23:26:02 +0800

    Rename duplicate column to Subtotal

--- db/dbo.Transaction.sql
+++ db/dbo.Transaction.sql
@@ -2,7 +2,7 @@
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
     [Date] DATETIME NULL, 
-    [Total] INT NULL, 
+    [Subtotal] INT NULL, 
     [Cash] INT NULL, 
     [DiscountPercent] INT NULL, 
     [DiscountAmount] DECIMAL(18, 2) NULL, 
