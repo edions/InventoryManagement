e1b643d697179768bd8b78c6785e3482864a6acb
EdisonP <133362311+edions@users.noreply.github.com>
2023-06-07 18:31:40 +0800

    New db query

---
+++ db/Database.sql
@@ -0,0 +1,48 @@
+﻿CREATE TABLE [dbo].[Product]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
+    [Name] VARCHAR(50) NULL, 
+    [Price] INT NULL, 
+    [Stock] INT NULL, 
+    [Unit] INT NULL, 
+    [Category] VARCHAR(50) NULL
+)
+CREATE TABLE [dbo].[Category]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
+    [CategoryItem] VARCHAR(50) NULL,
+)
+CREATE TABLE [dbo].[History]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
+    [ProductID] INT NULL, 
+    [Added Stocks] INT NULL, 
+    [Date] DATETIME NULL,
+)
+CREATE TABLE [dbo].[Cart]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
+    [Name] VARCHAR(50) NULL, 
+    [Price] INT NULL, 
+    [Quantity] INT NULL
+)
+CREATE TABLE [dbo].[Transaction]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
+    [Date] DATETIME NULL, 
+    [Subtotal] INT NULL, 
+    [Cash] INT NULL, 
+    [DiscountPercent] VARCHAR(50) NULL, 
+    [DiscountAmount] DECIMAL(18, 2) NULL, 
+    [Total] INT NULL, 
+    [Change] DECIMAL(18, 2) NULL, 
+    [TransactionId] VARCHAR(MAX) NULL,
+)
+CREATE TABLE [dbo].[Orders]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
+    [TransactionId] VARCHAR(MAX) NULL, 
+    [Name] VARCHAR(50) NULL, 
+    [Price] INT NULL, 
+    [Quantity] INT NULL
+)
