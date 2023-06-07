53ebd83bbf6daa046442083c88d071128c438c08
EdisonP <133362311+edions@users.noreply.github.com>
2023-06-07 18:41:58 +0800

    Added descriptions

--- db/Database.sql
+++ db/Database.sql
@@ -1,4 +1,5 @@
-﻿CREATE TABLE [dbo].[Product]
+-- Table to store product information
+CREATE TABLE [dbo].[Product]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
     [Name] VARCHAR(50) NULL, 
@@ -6,26 +7,34 @@
     [Stock] INT NULL, 
     [Unit] INT NULL, 
     [Category] VARCHAR(50) NULL
-)
+);
+
+-- Table to store category information
 CREATE TABLE [dbo].[Category]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
     [CategoryItem] VARCHAR(50) NULL,
-)
+);
+
+-- Table to track the history of stock updates
 CREATE TABLE [dbo].[History]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
     [ProductID] INT NULL, 
     [Added Stocks] INT NULL, 
     [Date] DATETIME NULL,
-)
+);
+
+-- Table to store items in the shopping cart
 CREATE TABLE [dbo].[Cart]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
     [Name] VARCHAR(50) NULL, 
     [Price] INT NULL, 
     [Quantity] INT NULL
-)
+);
+
+-- Table to store transaction information
 CREATE TABLE [dbo].[Transaction]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
@@ -37,7 +46,9 @@ CREATE TABLE [dbo].[Transaction]
     [Total] INT NULL, 
     [Change] DECIMAL(18, 2) NULL, 
     [TransactionId] VARCHAR(MAX) NULL,
-)
+);
+
+-- Table to store ordered items in a transaction
 CREATE TABLE [dbo].[Orders]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
@@ -45,4 +56,4 @@ CREATE TABLE [dbo].[Orders]
     [Name] VARCHAR(50) NULL, 
     [Price] INT NULL, 
     [Quantity] INT NULL
-)
+);
