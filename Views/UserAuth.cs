ea5e3ec2f0d1811dcf61a26ac8df4c82ad72ec42
EdisonP <133362311+edions@users.noreply.github.com>
2023-06-28 11:10:23 +0800

    Separate accounts the business logic

--- Views/UserAuth.cs
+++ Views/UserAuth.cs
@@ -1,37 +1,24 @@
 ﻿using InventoryApp.InventoryApp;
+using InventoryApp.Managers;
 using System;
-using System.Data.SqlClient;
 using System.Windows.Forms;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
 
 namespace InventoryApp.Views
 {
     public partial class UserAuth : Form
     {
         private bool isRegisterMode = false;
+        private readonly AccountManager accountManger;
         public UserAuth()
         {
             InitializeComponent();
+            accountManger = new AccountManager();
         }
 
         //Validate Users Credentials
         private bool ValidateUserCredentials(string username, string password)
         {
-            string connectionString = ConnectionManager.GetConnection().ConnectionString;
-
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                connection.Open();
-
-                string query = "SELECT COUNT(*) FROM Users WHERE Username = @Username AND Password = @Password";
-                SqlCommand command = new SqlCommand(query, connection);
-                command.Parameters.AddWithValue("@Username", username);
-                command.Parameters.AddWithValue("@Password", password);
-
-                int count = (int)command.ExecuteScalar();
-
-                return (count > 0);
-            }
+            return accountManger.ValidateUserCredentials(username, password);
         }
 
         // Login or Register Button
@@ -63,55 +50,7 @@ namespace InventoryApp.Views
         // Register new user
         private void RegisterUser(string username, string password)
         {
-            // Check if the username already exists in the database
-            if (IsUsernameExists(username))
-            {
-                MessageBox.Show("Username already exists. Please choose a different username.", "Registration Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return;
-            }
-
-            // Save the new user to the database
-            string connectionString = ConnectionManager.GetConnection().ConnectionString;
-
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                connection.Open();
-
-                string query = "INSERT INTO Users (Username, Password) VALUES (@Username, @Password)";
-                SqlCommand command = new SqlCommand(query, connection);
-                command.Parameters.AddWithValue("@Username", username);
-                command.Parameters.AddWithValue("@Password", password);
-
-                int rowsAffected = command.ExecuteNonQuery();
-
-                if (rowsAffected > 0)
-                {
-                    MessageBox.Show("Registration successful!", "Registration", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                }
-                else
-                {
-                    MessageBox.Show("Failed to register. Please try again.", "Registration Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-            }
-        }
-
-        // Check if user name already exists
-        private bool IsUsernameExists(string username)
-        {
-            string connectionString = ConnectionManager.GetConnection().ConnectionString;
-
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                connection.Open();
-
-                string query = "SELECT COUNT(*) FROM Users WHERE Username = @Username";
-                SqlCommand command = new SqlCommand(query, connection);
-                command.Parameters.AddWithValue("@Username", username);
-
-                int count = (int)command.ExecuteScalar();
-
-                return (count > 0);
-            }
+            accountManger.RegisterUser(username, password);
         }
 
         // Toggle label mode
